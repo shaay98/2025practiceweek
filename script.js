8e5acd70bee858bceb6943225c51f9737f811329
shaay98 <nrichardson567@icloud.com>
2025-01-02 04:26:59 +0000

    practice

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+
+let age prompt("Enter your age")
+
+if age >=18 {
+    alert Welcome to my favorite movie site.
+} else if age <18 {
+    alert You must be at least 18 years or older to enter site.
+}
+
+
